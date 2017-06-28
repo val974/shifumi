7cf3ad8f7027eca2334ef9b0251db57fe7a82122
val974 <valerie.dij@gmail.com>
2017-06-28 14:48:44 +0400

    ajout du score

--- shifumi.js
+++ shifumi.js
@@ -1,5 +1,6 @@
 const choix=["pierre", "feuille", "ciseaux"];
 let point = 0;
+let pointia = 0;
 
 
 let elements = document.querySelectorAll('img');
@@ -12,9 +13,12 @@ pierre.addEventListener('click', function(){
 	if(choix[0] && choix[ia] === choix[1]){
 		
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi feuille, vous avez PERDU";
+		document.getElementById('score2').innerHTML = pointia += 5;
 	}
 	else if(choix[0] && choix[ia] === choix[2]){
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi ciseaux, vous avez GAGNÉ";
+		document.getElementById('score1').innerHTML = point += 5;
+
 	}
 	else{
 		document.getElementById('txt1').innerHTML = 'EGALITÉ';
@@ -29,9 +33,12 @@ feuille.addEventListener('click', function(){
 	let ia = Math.round(Math.random() * 3);
 	if(choix[1] && choix[ia] === choix[2]){
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi ciseaux, vous avez PERDU";
+
+		document.getElementById('score2').innerHTML = pointia += 5;
 	}
 	else if(choix[1] && choix[ia] === choix[0]){
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi pierre, vous avez GAGNÉ";
+		document.getElementById('score1').innerHTML = point += 5;
 	}
 	else{
 		document.getElementById('txt1').innerHTML = 'EGALITÉ';
@@ -46,9 +53,11 @@ ciseaux.addEventListener('click', function(){
 	let ia = Math.round(Math.random() * 3);
 	if(choix[2] && choix[ia] === choix[0]){
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi pierre, vous avez PERDU";
+		document.getElementById('score2').innerHTML = pointia += 5;
 	}
 	else if(choix[2] && choix[ia] === choix[1]){
 		document.getElementById('txt1').innerHTML = "l'ordinateur a choisi feuille, vous avez GAGNÉ";
+		document.getElementById('score1').innerHTML = point += 5;
 	}
 	else{
 		document.getElementById('txt1').innerHTML = 'EGALITÉ';
